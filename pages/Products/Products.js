b16b517699f9985322ae1e2d6eb4f24c2495b5a6
mrroot <baisalkalykov69@gmail.com>
2023-05-29 21:41:12 +0600

    add alibek

--- pages/Products/Products.js
+++ pages/Products/Products.js
@@ -1,4 +1,23 @@
 
+let post = (el)=>{
+    fetch('http://localhost:3000/basket', {
+        method: 'POST',
+        headers: {
+           'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "id": el.id,
+            "image": el.image,
+            "name": el.name,
+            "title": el.title,
+            "category": el.category,
+            "gender": el.gender,
+            "color": el.color,
+            "price": el.price
+        })
+    })
+}
+
 
 let box = document.querySelector('.box')
 fetch('http://localhost:3000/shoes')
@@ -6,18 +25,21 @@ fetch('http://localhost:3000/shoes')
 .then((json)=>json.forEach((el)=>{
     box.innerHTML+=`
     <div class="card">
-    <img src="../../${el.image}" alt="">
+    <a href="http://127.0.0.1:5500/pages/singleProduct/single.html#${el.id}">
+    <img src="../../${el.image[0]}" alt="">
+    </a>
     <h1 class="name__h1">${el.name}</h1>
     <p class="title">${el.title}</p>
     <p class="category__p">${el.category}</p>
     <p class="category__p">${el.gender}</p>
     <p class="color__p">${el.color}</p>
     <p class="price">${el.price}</p>
-    <a href="../basket/korzina.html"><button class="card__btn">Корзину</button></a>
+    <button type="button" onClick="${()=>post(el)}" class="card__btn">Корзину</button>
 
 </div>
     `
 }))
+
 let men =document.querySelector('.header__center__ul__li__men')
 men.addEventListener('click',()=>{
     box.innerHTML=''
@@ -35,7 +57,7 @@ men.addEventListener('click',()=>{
     <p class="category__p">${el.gender}</p>
     <p class="color__p">${el.color}</p>
     <p class="price">${el.price}</p>
-    <button> ${Корзину}</button>
+    <button > ${Корзину}</button>
 
      </div>      `
         console.log(box)
@@ -51,9 +73,13 @@ women.addEventListener('click',()=>{
     .then((json)=>json.filter((el)=>{
         return el.gender === "Women"
     }).forEach(el=> {
+    
         box.innerHTML+=`
     <div class="card">
+    <a href="http://127.0.0.1:5500/pages/singleProduct/single.html#${el.id}">
     <img src="../../${el.image}" alt="">
+    </a>
+    
     <h1 class="name__h1">${el.name}</h1>
     <p class="title">${el.title}</p>
     <p class="category__p">${el.category}</p>
@@ -62,7 +88,7 @@ women.addEventListener('click',()=>{
     <p class="price">${el.price}</p>
 
      </div>      `
-        console.log(box)
+       
     }
     ))
 })
--- pages/singleProduct/single.js
+++ pages/singleProduct/single.js
@@ -1,36 +1,85 @@
-let container = document.querySelector('.single .container')
-fetch('http://localhost:3000/shoes/2')
-.then((rec)=>rec.json())
-.then((json)=>{
-    container.innerHTML+=`
-     <div class="nike__img__left">
-                <img class="nike__img1" src="./img/Frame.png" alt="">
-            
-            </div>
-            <div class="nike__right">
-                <h1>${json.title}</h1>
-                <h3>${json.category}</h3>
-                <h4>${json.price}</h4>
-                <p
-                <p>incl. of taxes<br>(Also includes all applicable duties)</p>
-                <div class="size">
 
-                    <h5>Select Size</h5>
-                    <div>
-                        <ul>
-                            <li>40</li>
-                            <li>41</li>
-                            <li>42</li>
-                        </ul>
+
+
+
+
+let container = document.querySelector('.single .container')
+
+
+
+let db = fetch(`http://localhost:3000/shoes/${location.hash.slice(1)}`)
+db.then(res=>res.json())
+.then((json)=>getInfo(json))
+
+
+
+let getInfo = (json)=>{
+
+   
+    container.innerHTML+=`
+         <div class="nike__img__left">
+         
+                   ~
+                
+                </div>
+                <div class="nike__right">
+                    <h1>${json.title}</h1>
+                    <h3>${json.category}</h3>
+                    <h4>${json.price}</h4>
+                    <p
+                    <p>incl. of taxes<br>(Also includes all applicable duties)</p>
+                    <div class="size">
+    
+                        <h5>Select Size</h5>
+                        <div>
+                            <ul>
+                                <li>40</li>
+                                <li>41</li>
+                                <li>42</li>
+                            </ul>
+                        </div>
+                        
+    
+    
                     </div>
+                    <button class ="size__btn1">Add to Bag</button>
+                    <button class ="size__btn2">Favourite</button>
+    
+                </div>`
+}
+
+// fetch('http://localhost:3000/shoes/2')
+// .then((rec)=>rec.json())
+// .then((json)=>{
+//     container.innerHTML+=`
+//      <div class="nike__img__left">
+//                 <img class="nike__img1" src="./img/Frame.png" alt="">
+            
+//             </div>
+//             <div class="nike__right">
+//                 <h1>${json.title}</h1>
+//                 <h3>${json.category}</h3>
+//                 <h4>${json.price}</h4>
+//                 <p
+//                 <p>incl. of taxes<br>(Also includes all applicable duties)</p>
+//                 <div class="size">
+
+//                     <h5>Select Size</h5>
+//                     <div>
+//                         <ul>
+//                             <li>40</li>
+//                             <li>41</li>
+//                             <li>42</li>
+//                         </ul>
+//                     </div>
                     
 
 
-                </div>
-                <button class ="size__btn1">Add to Bag</button>
-                <button class ="size__btn2">Favourite</button>
+//                 </div>
+//                 <button class ="size__btn1">Add to Bag</button>
+//                 <button class ="size__btn2">Favourite</button>
 
-            </div>
-    `
+//             </div>
+//     `
 
-})
+// })
